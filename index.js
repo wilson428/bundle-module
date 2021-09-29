901b05299322bec642e313909d70116adb84f083
Chris Wilson <christopher.e.wilson>
2021-09-29 13:50:14 -0400

    v0.1.8, with assist from @aatishdubey

--- index.js
+++ index.js
@@ -12,9 +12,7 @@ const bundleModule = function(args) {
 		entry: args.entry || "./index.js",
 		output: {
 			path: path.resolve(process.cwd(), args.output_dir || "./dist"),
-			libraryExport: args.export_name || undefined,
-			library: args.library || 'MyBundle',
-        	libraryTarget: args.libraryTarget || 'umd',
+			libraryExport: args.export_name || "default"
 		},
 		minified: (args.min || args.minified) ? true : false,
 		source_map: args.source_map || (args.min ? false : true ),
